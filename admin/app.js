cbec13f0df5ed34c4e7d249cb29489f42de494b4
CarlosProgrammer45 <cpc417@gmail.com>
2026-05-26 10:07:09 +0100

    Update app.js

--- admin/app.js
+++ admin/app.js
@@ -49,7 +49,7 @@ app.use(session({
 }));
 
 const path = require('path');
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.static(path.join(__dirname, '../dist/Angular20/browser')));
 app.use('/api', routes);
